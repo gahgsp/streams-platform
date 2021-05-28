1486210ceb2eecb9e0c11b010cc9f5bbda25c992
gahgsp <kelvinbianchi@gmail.com>
2021-05-28 18:50:07 -0300

    Implemented a generic form component to reuse the code in both create and edit pages.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,56 +1,21 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions/index';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-  renderInput = ({ input, label, meta }) => {
-    // This 'meta' has properties from the field such as invalid, dirty, touched, error...
-    return (
-      <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
-        <label>{label}</label>
-        <input {...input} />
-        {this.renderErrorMessage(meta)}
-      </div>
-    );
-  };
-
-  renderErrorMessage = ({ error, touched }) => {
-    if (error && touched) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-
   onSubmit = (formValues) => {
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field name="title" component={this.renderInput} label="Title" />
-        <Field name="description" component={this.renderInput} label="Description" />
-        <button className="ui button primary">Create</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validationsHandler = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = 'The title is a required field!';
-  }
-  if (!formValues.description) {
-    errors.description = 'The description is a required field!';
-  }
-  return errors;
-};
-
-const wrappedForm = reduxForm({ form: 'streamCreate', validate: validationsHandler })(StreamCreate);
-
-export default connect(null, { createStream })(wrappedForm);
+export default connect(null, { createStream })(StreamCreate);
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,14 +1,28 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { editStream, fetchStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
-    return <div>Stream Edit is working!</div>;
+    const streamInitialValues = this.props.stream ? _.pick(this.props.stream, 'title', 'description') : null;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        {/* The "initialValues" property is a built-in property from ReduxForm. */}
+        {/* If this property is defined, it will automatically fill the fields with the same names as the object. */}
+        <StreamForm initialValues={streamInitialValues} onSubmit={this.onSubmit}></StreamForm>
+      </div>
+    );
   }
 }
 
@@ -16,4 +30,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { editStream, fetchStream })(StreamEdit);
