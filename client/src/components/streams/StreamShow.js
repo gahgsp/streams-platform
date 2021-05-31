ecc8658efdf59def9a9f80435a2440483427fd11
gahgsp <kelvinbianchi@gmail.com>
2021-05-31 19:12:42 -0300

    Implemented the video player on the stream show page. The video is streamed by using OBS.

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,50 @@
+import flv from 'flv.js';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+
+    // After first rendering the component, tries to build the video player.
+    // This helps at least showing the video player when the user refreshes the page
+    // and we do not still have the stream fetched from the store.
+    this.buildVideoPlayer();
+  }
+
+  componentDidUpdate() {
+    // After fetching successfully the stream from the store,
+    // we attempt to build the video player again but this time
+    // with all the information.
+    this.buildVideoPlayer();
+  }
+
+  componentWillUnmount() {
+    this.videoPlayer.destroy();
+  }
+
+  buildVideoPlayer() {
+    if (this.videoPlayer || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    this.videoPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.videoPlayer.attachMediaElement(this.videoRef.current);
+    this.videoPlayer.load();
   }
 
   render() {
@@ -16,6 +56,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
